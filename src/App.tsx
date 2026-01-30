488e02bbdcaff45fec9fda36fe60c2126d3986ad
GraceC-326 <grace.a.cochran@maine.edu>
2026-01-30 16:59:51 -0500

    Updated App.css/.tsx, Added one image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import img from "./Daemon_bugInTheIce.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,12 +9,40 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <h1>Grace Cochran</h1>;
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
             <p>Hello World</p>
-            <p>Grace Cochran</p>
+            <ol>
+                <li>
+                    Step 1: Learn CSS, HTML, and JS/TS from this class and
+                    others
+                </li>
+                <li>Step 2: Make a Neocities page with the knowledge gained</li>
+                <li>Step 3: Feel Self Fulfilled</li>
+            </ol>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <img
+                src={img}
+                alt="An image of Daemon from the video game Date Everything. They are seen with their lower body stuck under a sheet of lake ice, a shocked expression on its face."
+            />
+            <div className="App" style={{width: 50, height: 40, backgroundColor: "red" }}>
+                <Container>
+                    <Row>
+                        <Col>a</Col>
+                        <Col>a</Col>
+                    </Row>
+                </Container>
+            </div>
+            ;
         </div>
     );
 }
