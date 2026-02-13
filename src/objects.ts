24208e5e69f43aa670a5668f683b8906ed43c72a
GraceC-326 <grace.a.cochran@maine.edu>
2026-02-13 16:04:52 -0500

    Updated objects.ts

--- src/objects.ts
+++ src/objects.ts
@@ -111,6 +111,7 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
+    oldQuestion = { ...oldQuestion, id: id, name: "Copy of " + oldQuestion.name, published: false };
     return oldQuestion;
 }
 
