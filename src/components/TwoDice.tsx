a6436ff90ce1ec9cf53c675a38bb065324f0937a
GraceC-326 <grace.a.cochran@maine.edu>
2026-02-27 19:05:15 -0500

    Added instructive comments to components

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -14,3 +14,17 @@ export function d6(): number {
 export function TwoDice(): React.JSX.Element {
     return <div>Two Dice</div>;
 }
+
+
+/*
+The TwoDice component will simulate a game where you roll two dice in an attempt to get matching values. However, you lose the game if your dice ever come up as a pair of ones (“snake eyes”).
+
+- You will need two states, one for each die.
+- Each die’s value should be rendered in the View in a span tag of their own, with the first dice having the data-testid of left-die and the second dice having the data-testid of right-die.
+- You will need two “Roll” buttons (labelled Roll Left and Roll Right).
+- Clicking a Roll button will change the value for the corresponding dice using the provided d6 function.
+- Make the default initial values of the dice be different, i.e. each value in useState(value)
+- When the two states are equal and equal to 1, render a message that includes the word Lose.
+- When the two states are equal, render a message that includes the word Win.
+- If you do all these and are still not passing all tests, read the test file, it is a good practice! In much of software engineering, the tests are the product spec.
+*/
